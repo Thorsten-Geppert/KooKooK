6e09bcc135a9b4db10bfb12830864eea2e4bcfb4
Thorsten-Geppert <76630161+Thorsten-Geppert@users.noreply.github.com>
2022-11-29 21:02:39 +0100

    Advanced compile.sh

--- src/modules/server/Server.pro
+++ src/modules/server/Server.pro
@@ -2,3 +2,5 @@ HEADERS += Server.h ServerThread.h ServerThreadList.h ServerThreadManager.h
 SOURCES += main.cpp Server.cpp ServerThread.cpp ServerThreadList.cpp ServerThreadManager.cpp
 QT += network
 CONFIG += console c++11
+Debug:DESTDIR = debug
+Release:DESTDIR = release
--- src/modules/server/compile.sh
+++ src/modules/server/compile.sh
@@ -1,2 +1,29 @@
 #!/bin/sh
-~/Qt/6.3.1/macos/bin/qmake6 && make
+
+QMAKE="/Users/thorsten/Qt/6.4.1/macos/bin/qmake6"
+LAST_PATH="`pwd`"
+COMPILE_PATH="`dirname $0`/build"
+
+if [ $# -gt 0 ]
+then
+	if [[ $1 == "--clean" ]]
+	then
+		test -d "${COMPILE_PATH}" && rm -Rf "${COMPILE_PATH}"
+	else
+		echo "Usage: ${0} [--clean | --help]"
+	fi
+else
+	if [ ! -d "${COMPILE_PATH}" ]
+	then
+		mkdir -p "${COMPILE_PATH}"
+		if [ $? != 0 ]
+		then
+			echo "Could not create build directory ${COMPILE_PATH}"
+			exit 1
+		fi
+	fi
+
+	cd "${COMPILE_PATH}"
+	"${QMAKE}" ../ && make
+	cd "${LAST_PATH}"
+fi
